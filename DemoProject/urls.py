d1d778d610d73229b16b0071993a4dd39e787511
Deep Savaliya <154747338+DeepSavaliya1904@users.noreply.github.com>
2025-09-15 13:24:09 +0530

    Update urls.py

--- DemoProject/urls.py
+++ DemoProject/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Apk1 import views
+from Apk1.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('index/',views.indexPage),
+    path('index/',indexPage),
 ]
