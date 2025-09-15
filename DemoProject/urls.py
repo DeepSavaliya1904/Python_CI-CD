db300227afe871bc4b1cca15393da072ec25ac99
Deep Savaliya <154747338+DeepSavaliya1904@users.noreply.github.com>
2025-09-15 11:11:15 +0530

    add login page

--- DemoProject/urls.py
+++ DemoProject/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from Apk1 import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/',views.indexPage)
 ]
