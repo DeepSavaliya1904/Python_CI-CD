729cc7758c98edb71ddb36cbe1d4bbb9e4fd988a
Deep Savaliya <154747338+DeepSavaliya1904@users.noreply.github.com>
2025-09-15 13:33:47 +0530

    Update urls.py

--- DemoProject/urls.py
+++ DemoProject/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from Apk1.views import *
+from django.urls import path, include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('index/',indexPage),
+    path('', include('Apk1.urls')),
 ]
